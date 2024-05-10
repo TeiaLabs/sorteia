7ac22d5b88c779156cd15c05b424d9b1adc065c2
Kristen <kristenarguello20@gmail.com>
2024-05-09 21:46:59 -0300

    Add model to custom sortings collection

--- sorteia/models.py
+++ sorteia/models.py
@@ -1,31 +1,23 @@
-from dataclasses import Field
-import datetime
-from bson import ObjectId
-from tauth.schemas import Creator
+from typing import Any, Collection, Optional
+from pymongo import IndexModel
+from redbaby.document import Document
+from .schemas import CustomSorting, CustomSortingWithResource
+from redbaby.database import DB
 
-class DBRef:
-  collection: str = Field(alias="$ref") 
-  id: ObjectId = Field(alias="$id")
+class CustomSortingDAO(Document, CustomSorting):
+    @classmethod
+    def collection_name(cls) -> str:
+        return "custom-sortings"
 
-  class Config: 
-    allow_population_by_field_name = True
-    json_encoders = {ObjectId: lambda oid: str(oid)}
+    @classmethod
+    def collection(cls, suffix: Optional[str] = None) -> Collection:
+        return DB.get(suffix=suffix)[cls.collection_name()]
 
+    @classmethod
+    def indexes(cls) -> list[IndexModel]:
+        return [{"resource_ref.$ref": 1, "position": 1}]
 
-class CustomSorting:
-  # id: Hash(created_by.user_email, resource_ref.$ref, position)
-  created_at: datetime
-  updated_at: datetime
-  created_by: Creator
-  position: int
-  resource_ref: DBRef
-                
-  indexes = [{"resource_ref.$ref": 1, "position": 1}] 
-
-class CustomSortingWithResource[T]: 
-  id: ObjectId
-  created_at: datetime
-  updated_at: datetime
-  created_by: Creator
-  position: int
-  resource: T #actual resource
+    def bson(self) -> dict[str, Any]:
+        obj = self.model_dump(by_alias=True)
+        return obj
+    
