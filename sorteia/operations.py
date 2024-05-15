298de4d33017d438007401772e9e05a072532c84
Kristen <kristenarguello20@gmail.com>
2024-05-15 20:30:49 -0300

    Fix operations init and type hint it

--- sorteia/operations.py
+++ sorteia/operations.py
@@ -1,17 +1,24 @@
-from datetime import datetime
 import os
-from annotated_types import T
-from bson import ObjectId
-from fastapi import BackgroundTasks
 import pymongo
-import dotenv
 
-from redbaby.database import DB
-from tauth.schemas import Creator
+from typing import Any
+from bson import ObjectId
+from datetime import datetime
+from annotated_types import T
+from fastapi import BackgroundTasks
+from dotenv import load_dotenv 
 
-from .schemas import CustomSorting, CustomSortingWithResource
-from .exceptions import CustomOrderNotSaved
-from .models import CustomSortingDAO
+import pymongo.command_cursor
+import pymongo.cursor
+import pymongo.results
+from redbaby.database import DB # type: ignore
+from tauth.schemas import Creator # type: ignore
+
+from .models import CustomSorting, CustomSortingWithResource
+from .schemas import ReorderResourcesIn
+from .exceptions import CustomOrderNotFound, CustomOrderNotSaved
+
+load_dotenv()
 
 # TODO: add on env and settings
 MONGO_URI = os.getenv("MONGO_URI")
@@ -32,8 +39,11 @@ class Sortings:
         """
         collection_name: collection name of the elements to be sorted
         """
-        self.collection = collection_name
-        self.sortings = DB.get()["custom-sortings"]
+        print("Creating the sortings thingy")
+        self.collection : str = collection_name
+        self.sortings = DB.get()["custom-sortings"] # type: ignore
+        self.create_search_indexes()
+        print("Created the sortings thingy")
 
     def reorder_one(self, creator: Creator, resource_id: ObjectId, position: int):
         """
@@ -69,7 +79,7 @@ class Sortings:
 
     def reorder_many(
         self,
-        resources: list,
+        resources: list[ReorderResourcesIn],
         creator: Creator,
     ):
         self.sortings.bulk_write(
@@ -101,7 +111,8 @@ class Sortings:
         Returns the objects in the order they were sorted.
         """
 
-        custom_sortings = self.sortings.find(
+        # TODO: change this Any to specific type
+        custom_sortings : pymongo.cursor.Cursor[Any] = self.sortings.find(
             filter={
                 "resource_ref.$ref": self.collection,
             }
@@ -114,7 +125,8 @@ class Sortings:
         Returns the objects in the order they were sorted.
         """
 
-        custom_sortings = self.sortings.aggregate(
+        # TODO: change this Any to specific type
+        custom_sortings : pymongo.command_cursor.CommandCursor[Any] = self.sortings.aggregate(
             [
                 {
                     "$match": {
@@ -140,18 +152,18 @@ class Sortings:
 
     def delete_one(
         self, position: int, creator: Creator, background_task: BackgroundTasks
-    ):
+    ) -> pymongo.results.DeleteResult:
         """
         position: int position to be deleted
         background_task: BackgroundTasks object (comes from route dependency)
         """
-        user_email = creator.user_email
+        # user_email = creator.user_email
 
         filter = {"position": position}
-        result = self.sortings.delete_one(filter)
+        result : pymongo.results.DeleteResult = self.sortings.delete_one(filter)
 
         if result.deleted_count == 0:
-            raise
+            raise CustomOrderNotFound
 
         background_task.add_task(
             self.sortings.update_many,
@@ -164,4 +176,6 @@ class Sortings:
         """
         Creates the indexes needed for the custom sortings.
         """
-        CustomSortingDAO.create_indexes()
+        self.sortings.create_index([("resource.$ref", pymongo.ASCENDING)])
+        self.sortings.create_index([("position", pymongo.ASCENDING)])
+      
