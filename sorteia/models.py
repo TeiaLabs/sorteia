46aa1fedbefe46d86258660b5c9f91df0f7bf65c
Kristen <kristenarguello20@gmail.com>
2024-05-15 20:30:30 -0300

    Change models to use pymongo directly

--- sorteia/models.py
+++ sorteia/models.py
@@ -1,23 +1,32 @@
-from typing import Any, Collection, Optional
-from pymongo import IndexModel
-from redbaby.document import Document
-from .schemas import CustomSorting, CustomSortingWithResource
-from redbaby.database import DB
+from pydantic import BaseModel, Field
+from datetime import datetime
+from tauth.schemas import Creator # type: ignore
 
-class CustomSortingDAO(Document, CustomSorting):
-    @classmethod
-    def collection_name(cls) -> str:
-        return "custom-sortings"
+from sorteia.utils import PyObjectId
 
-    @classmethod
-    def collection(cls, suffix: Optional[str] = None) -> Collection:
-        return DB.get(suffix=suffix)[cls.collection_name()]
 
-    @classmethod
-    def indexes(cls) -> list[IndexModel]:
-        return [{"resource_ref.$ref": 1, "position": 1}]
+class DBRef(BaseModel):
+  collection: str = Field(alias="$ref") 
+  id: PyObjectId = Field(alias="$id")
 
-    def bson(self) -> dict[str, Any]:
-        obj = self.model_dump(by_alias=True)
-        return obj
-    
+  # class Config: 
+  #   populate_by_name = True
+  #   json_encoders = {PyObjectId: lambda oid: str(oid)}
+
+class CustomSorting(BaseModel):
+  id: PyObjectId = Field(alias="_id")
+  created_at: datetime
+  updated_at: datetime
+  created_by: Creator
+  position: int
+  resource_ref: DBRef
+  # id: Hash(created_by.user_email, resource_ref.$ref, position)
+  # indexes = [{"resource_ref.$ref": 1, "position": 1}]
+
+class CustomSortingWithResource[T](BaseModel): 
+  id: PyObjectId = Field(alias="_id")
+  created_at: datetime
+  updated_at: datetime
+  created_by: Creator
+  position: int
+  resource: T # actual resource 
