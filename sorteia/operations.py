f3de09a3f97c73d5c22c560724c5006142f46bc4
Kristen <kristenarguello20@gmail.com>
2024-05-15 21:49:14 -0300

    Make returned types better (add schemas)

--- sorteia/operations.py
+++ sorteia/operations.py
@@ -1,22 +1,21 @@
 import os
-import pymongo
-
-from typing import Any
-from bson import ObjectId
 from datetime import datetime
-from annotated_types import T
-from fastapi import BackgroundTasks
-from dotenv import load_dotenv 
+from typing import Any
 
+import pymongo
 import pymongo.command_cursor
 import pymongo.cursor
 import pymongo.results
-from redbaby.database import DB # type: ignore
-from tauth.schemas import Creator # type: ignore
+from annotated_types import T
+from dotenv import load_dotenv
+from fastapi import BackgroundTasks
+from redbaby.database import DB  # type: ignore
+from tauth.schemas import Creator  # type: ignore
 
-from .models import CustomSorting, CustomSortingWithResource
-from .schemas import ReorderResourcesIn
 from .exceptions import CustomOrderNotFound, CustomOrderNotSaved
+from .models import CustomSorting, CustomSortingWithResource
+from .schemas import ReorderManyResourcesIn, ReorderOneUpdatedOut, ReorderOneUpsertedOut
+from .utils import PyObjectId  # type: ignore
 
 load_dotenv()
 
@@ -40,12 +39,14 @@ class Sortings:
         collection_name: collection name of the elements to be sorted
         """
         print("Creating the sortings thingy")
-        self.collection : str = collection_name
-        self.sortings = DB.get()["custom-sortings"] # type: ignore
+        self.collection: str = collection_name
+        self.sortings = DB.get()["custom-sortings"]  # type: ignore
         self.create_search_indexes()
         print("Created the sortings thingy")
 
-    def reorder_one(self, creator: Creator, resource_id: ObjectId, position: int):
+    def reorder_one(
+        self, creator: Creator, resource_id: PyObjectId, position: int
+    ) -> ReorderOneUpsertedOut | ReorderOneUpdatedOut:
         """
         Reorders a resource in the custom order.
         creator: Creator object
@@ -58,31 +59,47 @@ class Sortings:
             "created_by.user_email": creator.user_email,
         }
 
+        updated_at = datetime.now()
+        created_at = datetime.now()
         custom_sorting = {
             "position": position,
             "resource_ref": {"$ref": self.collection, "$id": resource_id},
-            "updated_at": datetime.now(),
+            "updated_at": updated_at,
         }
         update = {
             "$set": custom_sorting,
             "$setOnInsert": {
-                "created_at": datetime.now(),
+                "created_at": created_at,
                 "created_by": creator.model_dump(by_alias=True),
             },
         }
 
         result = self.sortings.update_one(filter=filter, update=update, upsert=True)
-        if result.modified_count == 0 and result.upserted_id is None:
-            raise CustomOrderNotSaved
 
-        return result
+        if result.upserted_id is not None:
+            return ReorderOneUpsertedOut(
+                id=result.upserted_id,
+                created_at=created_at,
+                updated_at=updated_at,
+                created_by=creator,
+            )
+        elif result.modified_count == 1:
+            from rich import print
+
+            print(result.raw_result)
+            return ReorderOneUpdatedOut(
+                id=PyObjectId("hi"),
+                updated_at=updated_at,
+            )
+        else:
+            raise CustomOrderNotSaved
 
     def reorder_many(
         self,
-        resources: list[ReorderResourcesIn],
+        resources: list[ReorderManyResourcesIn],
         creator: Creator,
-    ):
-        self.sortings.bulk_write(
+    ) -> pymongo.results.BulkWriteResult:
+        result = self.sortings.bulk_write(
             [
                 pymongo.UpdateOne(
                     filter={
@@ -105,6 +122,7 @@ class Sortings:
                 for i, resource in enumerate(resources)
             ]
         )
+        return result
 
     def read_many(self) -> list[CustomSorting]:
         """
@@ -112,7 +130,7 @@ class Sortings:
         """
 
         # TODO: change this Any to specific type
-        custom_sortings : pymongo.cursor.Cursor[Any] = self.sortings.find(
+        custom_sortings: pymongo.cursor.Cursor[Any] = self.sortings.find(
             filter={
                 "resource_ref.$ref": self.collection,
             }
@@ -120,29 +138,33 @@ class Sortings:
 
         return [CustomSorting(**custom_sorting) for custom_sorting in custom_sortings]
 
-    def read_many_whole_object(self) -> list[CustomSortingWithResource[T]]:
+    def read_many_whole_object(
+        self,
+    ) -> list[CustomSortingWithResource[T]]:
         """
         Returns the objects in the order they were sorted.
         """
 
         # TODO: change this Any to specific type
-        custom_sortings : pymongo.command_cursor.CommandCursor[Any] = self.sortings.aggregate(
-            [
-                {
-                    "$match": {
-                        "resource_ref.$ref": self.collection,
-                    }
-                },
-                {
-                    "$lookup": {
-                        "from": self.collection,
-                        "localField": "resource_ref.$id",
-                        "foreignField": "_id",
-                        "as": "resource",
-                    }
-                },
-                {"$sort": {"position": pymongo.ASCENDING}},
-            ]
+        custom_sortings: pymongo.command_cursor.CommandCursor[Any] = (
+            self.sortings.aggregate(
+                [
+                    {
+                        "$match": {
+                            "resource_ref.$ref": self.collection,
+                        }
+                    },
+                    {
+                        "$lookup": {
+                            "from": self.collection,
+                            "localField": "resource_ref.$id",
+                            "foreignField": "_id",
+                            "as": "resource",
+                        }
+                    },
+                    {"$sort": {"position": pymongo.ASCENDING}},
+                ]
+            )
         )
 
         return [
@@ -160,7 +182,7 @@ class Sortings:
         # user_email = creator.user_email
 
         filter = {"position": position}
-        result : pymongo.results.DeleteResult = self.sortings.delete_one(filter)
+        result: pymongo.results.DeleteResult = self.sortings.delete_one(filter)
 
         if result.deleted_count == 0:
             raise CustomOrderNotFound
@@ -172,10 +194,9 @@ class Sortings:
         )
         return result
 
-    def create_search_indexes(self):
+    def create_search_indexes(self) -> None:
         """
         Creates the indexes needed for the custom sortings.
         """
         self.sortings.create_index([("resource.$ref", pymongo.ASCENDING)])
         self.sortings.create_index([("position", pymongo.ASCENDING)])
-      
--- sorteia/schemas.py
+++ sorteia/schemas.py
@@ -1,8 +1,38 @@
+from datetime import datetime
+
 from pydantic import BaseModel
+from tauth.schemas import Creator  # type: ignore
 
 from sorteia.utils import PyObjectId
 
-class ReorderResourcesIn(BaseModel):
-  resource_id: PyObjectId
-  resource_ref: str 
-  position: int
+
+class ReorderManyResourcesIn(BaseModel):
+    resource_id: PyObjectId
+    resource_ref: str
+    position: int
+
+    class Config:
+        examples = {
+            "resource_id": "resource.$id",
+            "resource_ref": "resource.$ref",
+            "position": 0,
+        }
+
+
+class ReorderOneResourceIn(BaseModel):
+    resource_id: PyObjectId
+
+    class Config:
+        examples = {"resource_id": "pyobjectid"}
+
+
+class ReorderOneUpsertedOut(BaseModel):
+    id: PyObjectId
+    created_at: datetime
+    updated_at: datetime
+    created_by: Creator
+
+
+class ReorderOneUpdatedOut(BaseModel):
+    id: PyObjectId
+    updated_at: datetime
