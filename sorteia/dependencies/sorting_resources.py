4380d384e68c1373fd26873d67f88c41dc45dbdf
Kristen <kristenarguello20@gmail.com>
2024-05-22 17:50:38 -0300

    fix some mistakes and add error handling on routes

--- sorteia/dependencies/sorting_resources.py
+++ sorteia/dependencies/sorting_resources.py
@@ -1,10 +1,25 @@
 from typing import Literal
 
 from annotated_types import T
-from fastapi import APIRouter, BackgroundTasks, Body, Depends, FastAPI, Header, Query
+from fastapi import (
+    APIRouter,
+    BackgroundTasks,
+    Body,
+    Depends,
+    FastAPI,
+    Header,
+    HTTPException,
+    Query,
+)
+from loguru import logger
 from tauth.injections import privileges  # type: ignore
 from tauth.schemas import Creator  # type: ignore
 
+from sorteia.exceptions import (
+    CustomOrderNotFound,
+    CustomOrderNotSaved,
+    ObjectToBeSortedNotFound,
+)
 from sorteia.models import CustomSorting, CustomSortingWithResource
 
 from ..operations import Sortings
@@ -27,7 +42,7 @@ def add_sorting_resources_dependency(app: FastAPI) -> None:
             default="no", alias="X-Resolve-Refs"
         ),
         creator: Creator = Depends(privileges.is_valid_user),
-    ) -> list[CustomSorting | CustomSortingWithResource[T]]:  # type: ignore
+    ) -> list[CustomSortingWithResource[T] | CustomSorting]:  # type: ignore
         """
         Returns the custom order of a resource.
 
@@ -39,9 +54,10 @@ def add_sorting_resources_dependency(app: FastAPI) -> None:
         `resolve_refs`: Literal["$resource_ref", "no"] resolve references
         `creator`: Creator object
         """
+        org = creator.user_email.split("@")[1].split(".")[0]
         if resolve_refs == "$resource_ref":
-            return Sortings(resource).read_many_whole_object(creator=creator)  # type: ignore
-        return Sortings(resource).read_many(creator=creator)  # type: ignore
+            return Sortings(collection_name=resource, alias=org, db_name=org).read_many_whole_object(creator=creator)  # type: ignore
+        return Sortings(collection_name=resource, alias=org, db_name=org).read_many(creator=creator)  # type: ignore
 
     @router.put("/{resource}/{position}", status_code=201)
     def reorder_one(  # type: ignore
@@ -63,16 +79,30 @@ def add_sorting_resources_dependency(app: FastAPI) -> None:
         {"resource_id": "pyobjectid"}
         ```
         """
-        return Sortings(resource).reorder_one(
-            creator=creator, resource_id=body.resource_id, position=position
-        )
+        org = creator.user_email.split("@")[1].split(".")[0]
+        try:
+            return Sortings(
+                collection_name=resource, alias=org, db_name=org
+            ).reorder_one(
+                creator=creator, resource_id=body.resource_id, position=position
+            )
+        except ObjectToBeSortedNotFound:
+            raise HTTPException(
+                status_code=404,
+                detail="Object to be sorted not found - ObjectToBeSortedNotFound",
+            )
+        except CustomOrderNotSaved:
+            raise HTTPException(
+                status_code=400,
+                detail="Custom order not saved - maybe because of an internal error - CustomOrderNotSaved",
+            )
 
     @router.delete("/{resource}/{position}", status_code=204)
     def delete_sorting(  # type: ignore
         resource: str,
         position: int,
-        creator: Creator,
         background_task: BackgroundTasks,
+        creator: Creator = Depends(privileges.is_valid_user),
     ) -> None:
         """
         Deletes a resource from the custom order.
@@ -81,7 +111,15 @@ def add_sorting_resources_dependency(app: FastAPI) -> None:
         `position`: int position to be deleted
         `creator`: Creator object
         """
-        Sortings(resource).delete_one(position, creator, background_task)
+        org = creator.user_email.split("@")[1].split(".")[0]
+        try:
+            Sortings(collection_name=resource, alias=org, db_name=org).delete_one(
+                position, creator, background_task
+            )
+        except CustomOrderNotFound:
+            raise HTTPException(
+                status_code=404, detail="Custom order not found - CustomOrderNotFound"
+            )
 
     @router.put("/{resource}", status_code=204)
     def reorder_many(  # type: ignore
@@ -102,6 +140,9 @@ def add_sorting_resources_dependency(app: FastAPI) -> None:
         }]
         ```
         """
-        Sortings(resource).reorder_many(resources=body, creator=creator)
+        org = creator.user_email.split("@")[1].split(".")[0]
+        Sortings(collection_name=resource, alias=org, db_name=org).reorder_many(
+            resources=body, creator=creator
+        )
 
     app.include_router(router)
